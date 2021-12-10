4fe6a3e8f5d26f99bc0858093afcec5c2137cf8f
elizaidoko <ochanya123@gmail.com>
2021-12-10 11:21:06 +0000

    done

--- app.js
+++ app.js
@@ -21,6 +21,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app_public')));
+app.use('/api', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+  });
+  
 
 app.use('/api', apiRoutes);
 app.use('/', index);
